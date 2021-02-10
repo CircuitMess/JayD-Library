5b45212c18d264f557af97f09cb979efa9959419
hrvojekolar <hrvojekolar1998@gmail.com>
2021-02-10 13:49:53 +0100

    added selected sprite

--- src/MainMenu/AppIcon.cpp
+++ src/MainMenu/AppIcon.cpp
@@ -1,15 +1,17 @@
 #include "AppIcon.hpp"
 
-AppIcon::AppIcon(ElementContainer *parent, const uint16_t *icon):CustomElement(parent,30,30), icon(icon){
+AppIcon::AppIcon(ElementContainer *parent, const uint16_t *icon) : CustomElement(parent, 53, 30), icon(icon){
 
 }
+
 void AppIcon::draw(){
 
-	getSprite()->fillRect(getTotalX(),getTotalY(),35,35,TFT_WHITE);
-	getSprite()->drawIcon(icon,getTotalX()+5,getTotalY()+5,30,30,1,TFT_TRANSPARENT);
-
+	getSprite()->fillRect(getTotalX(), getTotalY(), 63, 45, TFT_DARKGREY);
+	getSprite()->fillRect(getTotalX() + 2,getTotalY() + 2,60,40,selected ? TFT_RED : TFT_DARKGREY);
+	getSprite()->drawIcon(icon, getTotalX() + 5, getTotalY() + 5, 53, 35, 1, TFT_TRANSPARENT);
 	Element::draw();
 }
+
 void AppIcon::setSelected(bool selected){
 	AppIcon::selected = selected;
 }
