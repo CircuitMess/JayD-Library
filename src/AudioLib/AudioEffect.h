148f97c09264f1ab192d1dd28a47ef3fd87d58cc
Domagoj <domagoj.zoraja@fer.hr>
2021-02-19 15:58:56 +0100

    add setIntensity to AudioEffect

--- src/AudioLib/AudioEffect.h
+++ src/AudioLib/AudioEffect.h
@@ -6,6 +6,7 @@ class AudioEffect
 public:
 	virtual ~AudioEffect() = default;
 	virtual void applyEffect(int16_t *inBuffer, int16_t *outBuffer, int numBytes) = 0;
+	virtual void setIntensity(uint8_t intensity) = 0;
 };
 
 #endif
