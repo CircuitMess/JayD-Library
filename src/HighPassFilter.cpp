915d42027222eff932e84f07900bd155bf25ddab
Domagoj <domagoj.zoraja@fer.hr>
2021-02-23 14:09:25 +0100

    implement high pass filter

---
+++ src/HighPassFilter.cpp
@@ -0,0 +1,129 @@
+//
+// Created by Domagoj on 18/02/2021.
+//
+
+#include "HighPassFilter.h"
+
+HighPassFilter::HighPassFilter(){
+
+
+}
+
+void HighPassFilter::applyEffect(int16_t *inBuffer, int16_t *outBuffer, int numSamples){
+
+	for(int i = 0; i < numSamples/2; ++i){
+
+		*(outBuffer + i) = signalProcessing(*(inBuffer + i));
+	}
+}
+
+void HighPassFilter::idealFilterResponse(){
+
+	for(int n = -N/2; n <= N/2; ++n){
+
+		if(n==0){
+
+			idealFilterCoeffs[n+N/2] = 1 - (cutOffFrequency/PI);
+
+		}
+		else{
+
+			idealFilterCoeffs[n+N/2] = -sin((float)n*cutOffFrequency)/((float)n*PI);
+		}
+	}
+}
+
+void HighPassFilter::windowResponse(){
+
+	for(int n = -(L-1)/2; n <= (L-1)/2; ++n){
+
+		//windowCoeffs[n + (L-1)/2] = 0.42 + 0.5*cos((2*PI*n)/(L-1)) + 0.08*cos((4*PI*n)/(L-1));
+
+		float alpha = 0.5;
+		windowCoeffs[n + (L-1)/2] = 0.5 + (1-alpha)*cos((2*PI*n)/(L-1));	// Hann
+	}
+}
+
+void HighPassFilter::generateFilterCoeffs(){
+
+	idealFilterResponse();
+	windowResponse();
+
+	for(int n = 0; n < L; ++n){
+		coeffs[n] = windowCoeffs[n] * idealFilterCoeffs[n];
+	}
+
+}
+
+long HighPassFilter::circularIndex(long index, long increment, uint8_t irl){
+
+	index += increment;
+
+	if (index < 0){
+		index += irl;
+	}
+	else if(index >= irl){
+		index -= irl;
+	}
+
+	return index;
+}
+
+int16_t HighPassFilter::signalProcessing(int16_t sample){
+
+	int i;
+	int16_t acc = 0;
+
+	fDelay[j] = sample;
+
+	//j = (j+1) % L;
+	j = circularIndex(j, 1, L);
+
+	for(i = 0; i<L; ++i){
+
+		acc += coeffs[i]*(float)fDelay[j];
+		//j = (j+1) % L;
+		j = circularIndex(j, 1, L);
+	}
+
+	uint16_t maxAmp = pow(2,15)-1;
+	int32_t treshold = maxAmp * 0.9;
+	uint16_t window = maxAmp - treshold;
+
+	if(sample < 0){
+
+		treshold = -treshold;
+	}
+	//printf("%d %d\n", sample, acc);
+
+	if(abs(acc) >= abs(treshold)){
+
+		int16_t over = acc - treshold;
+		float overC = (float)over/(float)window;
+
+		acc = treshold + (float)over/(1.0/cos(overC*PI/2.0));
+
+		return acc;
+	}
+	else
+		return acc;
+}
+
+void HighPassFilter::setIntensity(uint8_t intensity){
+
+	//intensity = 1.0f / (float)intensity + 1;
+
+	intensity = (float)intensity/1.08f + 20;
+
+	cutOffFrequency = ((float)intensity/255.0f)*(float)PI;
+
+	if(intensity < 1){
+		intensity = 1;
+	}
+
+	gain = 4.0f/pow(intensity,2) + 1.0f;
+	//gain = (1.0f/((float)intensity+1.0f)) * 1.0f + 1;
+
+	generateFilterCoeffs();
+
+}
---
+++ src/HighPassFilter.h
@@ -0,0 +1,48 @@
+//
+// Created by Domagoj on 18/02/2021.
+//
+
+#ifndef JAYD_LIBRARY_HIGHPASSFILTER_H
+#define JAYD_LIBRARY_HIGHPASSFILTER_H
+
+#include "JayD.hpp"
+
+#define N 20		// filter order
+#define L 21		// length of the impulse response L = (N+1)
+
+class HighPassFilter: public AudioEffect {
+
+public:
+
+	HighPassFilter();
+
+	void applyEffect(int16_t *inBuffer, int16_t *outBuffer, int numSamples) override;
+
+	void setIntensity(uint8_t intensity) override;
+
+
+private:
+
+	void idealFilterResponse();
+	void windowResponse();
+
+	void generateFilterCoeffs();
+
+	long int circularIndex(long index, long increment, uint8_t irl);
+
+	int16_t signalProcessing(int16_t sample);
+
+	float cutOffFrequency = 0;
+	float gain = 0;
+
+	float coeffs[L] = {0};
+	float windowCoeffs[L] = {0};
+	float idealFilterCoeffs[L] = {0};
+
+	int16_t fDelay[L] = {0};
+
+	int j = 0;
+};
+
+
+#endif //JAYD_LIBRARY_HIGHPASSFILTER_H
