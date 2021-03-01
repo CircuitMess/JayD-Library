34a669395b711737f929beeecd0058596d7004d7
Domagoj <domagoj.zoraja@fer.hr>
2021-03-01 16:45:17 +0100

    update reverb

--- src/Reverb.cpp
+++ src/Reverb.cpp
@@ -1,11 +1,26 @@
-//
-// Created by Domagoj on 19/02/2021.
-//
-
 #include "Reverb.h"
 
 Reverb::Reverb(){
 
+	delaySamples = 80.0f;
+	decayFactor = 0.45f;
+
+	delayAllPassSamples = 90;
+	allPassDecay = 0.25f;
+
+	mixPercent = 0;
+
+	samplesBuffer = static_cast<int16_t *>(malloc(1024 * 50 * sizeof(int16_t)));
+	if(!samplesBuffer){
+		printf("Malloc ERROR");
+	}
+
+	for(int i = 0; i < 1024*50; ++i){
+		samplesBuffer[i] = 0;
+	}
+
+	sampleBufferCnt = 0;
+	startReverb = false;
 }
 
 
@@ -13,10 +28,10 @@ void Reverb::combFilter(int16_t* sample, int numberOfSamples){
 
 	/// Comb Filter: -> y[n] = x[n] + g·y[n−M]
 
-	int delayComb1Samples = (int) ((float)delaySamples);
-	int delayComb2Samples = (int) ((float)(delaySamples - 11.73f));
-	int delayComb3Samples = (int) ((float)(delaySamples + 19.31f));
-	int delayComb4Samples = (int) ((float)(delaySamples - 7.97f));
+	int delayComb1Samples = (int) ((float)(delaySamples * 44100.0f/1000.0f));
+	int delayComb2Samples = (int) ((float)(delaySamples + 8.0f) * 44100.0f/1000.0f);
+	int delayComb3Samples = (int) ((float)(delaySamples - 13.0f) * 44100.0f/1000.0f);
+	int delayComb4Samples = (int) ((float)(delaySamples + 17.0f) * 44100.0f/1000.0f);
 
 	combFilter1Output = static_cast<int32_t  *>(malloc(numberOfSamples * sizeof(int32_t)));
 	combFilter2Output = static_cast<int32_t  *>(malloc(numberOfSamples * sizeof(int32_t)));
@@ -49,76 +64,82 @@ void Reverb::allPassFilter(int32_t* samples, int numberOfSamples, uint8_t index)
 	// ‘M’ is the number of samples of delay.
 	// This is calculated from the delay (τ) and the sampling rate as — M = Sampling rate * τ
 
-	int delayAllPassSamples = (int) (float)89.27f;
-	int16_t M = SAMPLE_RATE*delayAllPassSamples;
-	float allPassDecay = 0.131f;
-
-	allPassFilter1Output = static_cast<int32_t  *>(malloc(numberOfSamples * sizeof(int32_t)));
-	allPassFilter2Output = static_cast<int32_t  *>(malloc(numberOfSamples * sizeof(int32_t)));
-
 	if(index == 1){
+
+		allPassFilter1Output = static_cast<int32_t  *>(malloc(numberOfSamples * sizeof(int32_t)));
 
 		for(int i = 0; i < (numberOfSamples); i++){
 
 			allPassFilter1Output[i] = samples[i];
-			//allPassFilter1Output[i+delayAllPassSamples] = -allPassDecay*allPassFilter1Output[i+delayAllPassSamples] + allPassFilter1Output[i] + allPassDecay*allPassFilter1Output[i];
-			//printf("AllPass1Out: %d\n", allPassFilter1Output[i+delayAllPassSamples]);
+			//allPassFilter1Output[i+delayAllPassSamples] = (int32_t)((float)(-allPassDecay*(float)allPassFilter1Output[i+delayAllPassSamples])) + allPassFilter1Output[i] + (int32_t)((float)(allPassDecay*(float)allPassFilter1Output[i]));
+			//printf("AllPass1In: %d\n", samples[i]);
 			if(i - delayAllPassSamples >= 0){
-				allPassFilter1Output[i] += (int32_t)(-allPassDecay * (float)allPassFilter1Output[i - delayAllPassSamples]);
+				allPassFilter1Output[i] += (int32_t)((float)(-allPassDecay * (float)allPassFilter1Output[i - delayAllPassSamples]));
 			}
 			if(i - delayAllPassSamples >= 1){
-				allPassFilter1Output[i] += (int32_t)(allPassDecay * (float)allPassFilter1Output[i + 20 - delayAllPassSamples]);
+				allPassFilter1Output[i] += (int32_t)((float)(allPassDecay * (float)allPassFilter1Output[i + 20 - delayAllPassSamples]));
 			}
 		}
 
-		int32_t value = allPassFilter1Output[0];
-		int32_t max = 0.0f;
 
-		for(int i=0; i < numberOfSamples; i++)
-		{
-			if(abs(allPassFilter1Output[i]) > max)
-				max = abs(allPassFilter1Output[i]);
-		}
+		uint16_t maxAmp = pow(2,15)-1;
+		int32_t treshold = maxAmp * 0.9;
+		uint16_t window = maxAmp - treshold;
 
 		for(int i=0; i<numberOfSamples; i++)
 		{
-			int32_t currentValue = allPassFilter1Output[i];
-			value = ((value + (currentValue - value))/max);
+			if(allPassFilter1Output[i] < 0){
 
-			allPassFilter1Output[i] = value;
+				treshold = -treshold;
+			}
+
+			if(abs(allPassFilter1Output[i]) >= abs(treshold)){
+
+				int32_t over = allPassFilter1Output[i] - treshold;
+				float overC = (float)over/(float)window;
+				allPassFilter1Output[i] = treshold + (float)over/(1.0/cos(overC*PI/2.0));
+			}
+			//printf("allPass1Out: %d\n", allPassFilter1Output[i]);
 		}
+
 	}
 	else if(index == 2){
 
-		for(int i = 0; i < (numberOfSamples ); i++){
+		allPassFilter2Output = static_cast<int32_t  *>(malloc(numberOfSamples * sizeof(int32_t)));
+
+		for(int i = 0; i < (numberOfSamples); i++){
 
 			allPassFilter2Output[i] = samples[i];
-			//allPassFilter2Output[i+delayAllPassSamples] = -allPassDecay*allPassFilter2Output[i+delayAllPassSamples] + allPassFilter2Output[i] + allPassDecay*allPassFilter2Output[i + 20];
+			//allPassFilter2Output[i+delayAllPassSamples] = (int32_t)((float)(-allPassDecay*(float)allPassFilter2Output[i+delayAllPassSamples])) + allPassFilter2Output[i] + (int32_t)((float)(allPassDecay*(float)allPassFilter2Output[i]));
+			//printf("AllPass2In: %d\n", samples[i]);
 			if(i - delayAllPassSamples >= 0){
-				allPassFilter2Output[i] += (int32_t)(-allPassDecay * (float)allPassFilter2Output[i - delayAllPassSamples]);
+				allPassFilter2Output[i] += (int32_t)((float)(-allPassDecay * (float)allPassFilter2Output[i - delayAllPassSamples]));
 			}
 			if(i - delayAllPassSamples >= 1){
-				allPassFilter2Output[i] += (int32_t)(allPassDecay * (float)allPassFilter2Output[i + 20 - delayAllPassSamples]);
+				allPassFilter2Output[i] += (int32_t)((float)(allPassDecay * (float)allPassFilter2Output[i + 20 - delayAllPassSamples]));
 			}
 		}
 
-		int32_t value = allPassFilter2Output[0];
-		int32_t max = 0.0f;
-
-		for(int i=0; i < numberOfSamples; i++)
-		{
-			if(abs(allPassFilter2Output[i]) > max)
-				max = abs(allPassFilter2Output[i]);
-		}
+		uint16_t maxAmp = pow(2,15)-1;
+		int32_t treshold = maxAmp * 0.9;
+		uint16_t window = maxAmp - treshold;
 
 		for(int i=0; i<numberOfSamples; i++)
 		{
-			int32_t currentValue = allPassFilter2Output[i];
-			value = ((value + (currentValue - value))/max);
+			if(allPassFilter2Output[i] < 0){
 
-			allPassFilter2Output[i] = value;
-			printf("allPassOut: %d\n", allPassFilter2Output[i]);
+				treshold = -treshold;
+			}
+
+			if(abs(allPassFilter2Output[i]) >= abs(treshold)){
+
+				int32_t over = allPassFilter2Output[i] - treshold;
+				float overC = (float)over/(float)window;
+				allPassFilter2Output[i] = treshold + (float)over/(1.0/cos(overC*PI/2.0));
+			}
+			//printf("allPass2Out: %d\n", allPassFilter2Output[i]);
 		}
+
 
 	}else
 		return;
@@ -132,47 +153,117 @@ void Reverb::signalProcessing(int16_t* sample, int numberOfSamples){
 
 	for(int i = 0; i < numberOfSamples; ++i){
 
-		combParallelOut[i] = combFilter1Output[i] + combFilter2Output[i] + combFilter3Output[i] + combFilter4Output[i];
+		combParallelOut[i] = (float)(combFilter1Output[i] + combFilter2Output[i] + combFilter3Output[i] + combFilter4Output[i])/4.0f;
+		//printf("Comb1Out: %d\n", combFilter1Output[i]);
 	}
 
 	int32_t* mixAudio = static_cast<int32_t *>(malloc(numberOfSamples * sizeof(int32_t)));
 
 	for(int i=0; i<numberOfSamples; i++){
-		mixAudio[i] = ((100 - mixPercent) * sample[i]) + (mixPercent * combParallelOut[i]);
+		mixAudio[i] = ((float)((100 - mixPercent) * sample[i])/100.0f + (float)(mixPercent * combParallelOut[i])/100.0f);
+		//printf("mixAudio: %d\n", mixAudio[i]);
+	}
+
+	allPassFilter2Output = static_cast<int32_t  *>(malloc(numberOfSamples * sizeof(int32_t)));
+	for(int i = 0; i < numberOfSamples; ++i){
+
+		allPassFilter2Output[i] = mixAudio[i];
+	}
+
+	uint16_t maxAmp = pow(2,15)-1;
+	int32_t treshold = maxAmp * 0.7;
+	uint16_t window = maxAmp - treshold;
+
+	for(int i=0; i<numberOfSamples; i++)
+	{
+		if(allPassFilter2Output[i] < 0){
+
+			treshold = -treshold;
+		}
+
+		if(abs(allPassFilter2Output[i]) >= abs(treshold)){
+
+			int32_t over = allPassFilter2Output[i] - treshold;
+			float overC = (float)over/(float)window;
+			allPassFilter2Output[i] = treshold + (float)over/(1.0/cos(overC*PI/2.0));
+		}
+		//printf("allPass2Out: %d\n", allPassFilter2Output[i]);
+	}
+
+	for(int i = 0; i < numberOfSamples; ++i){
+
+		samplesBuffer[i] = mixAudio[i];
 	}
 
 	free(combParallelOut);
 
-	allPassFilter(mixAudio, numberOfSamples, 1);
-	allPassFilter(allPassFilter1Output, numberOfSamples, 2);
+	//allPassFilter(mixAudio, numberOfSamples, 1);
+	//allPassFilter(allPassFilter1Output, numberOfSamples, 2);
 
 	free(mixAudio);
 }
 
 void Reverb::applyEffect(int16_t *inBuffer, int16_t *outBuffer, int numBytes){
 
-	signalProcessing(inBuffer , numBytes/2);
-
-	memcpy(outBuffer,allPassFilter2Output,numBytes/2);
-
 	for(int i = 0; i < numBytes/2; ++i){
-		printf("Out: %d\n", allPassFilter2Output[i]);
+		samplesBuffer[i + (numBytes/2)*sampleBufferCnt] = inBuffer[i];
 	}
+
+	uint16_t maxAmp = pow(2,15)-1;
+	int32_t treshold = maxAmp * 0.7;
+	uint16_t window = maxAmp - treshold;
+
+	if(startReverb){
+		for(int i=0; i<numBytes/2; i++){
+
+			outBuffer[i] = (1-((float)mixPercent/100.f))*(float)samplesBuffer[i + (numBytes/2)*sampleBufferCnt] + ((float)mixPercent/100.f)*(decayFactor*(float)samplesBuffer[i + (numBytes/2)*(sampleBufferCnt + 1)] + (decayFactor-0.13)*(float)samplesBuffer[i + (numBytes/2)*(sampleBufferCnt + 1)] + (decayFactor-0.27)*(float)samplesBuffer[i + (numBytes/2)*(sampleBufferCnt + 1)] + (decayFactor-0.31)*(float)samplesBuffer[i + (numBytes/2)*(sampleBufferCnt + 1)]);
+
+			if(outBuffer[i] < 0){
+
+				treshold = -treshold;
+			}
+
+			if(abs(outBuffer[i]) >= abs(treshold)){
+
+				int32_t over = outBuffer[i] - treshold;
+				float overC = (float)over/(float)window;
+				outBuffer[i] = treshold + (float)over/(1.0/cos(overC*PI/2.0));
+			}
+		}
+	}
+	else{
+		for(int i=0; i<numBytes/2; i++){
+
+			outBuffer[i] = samplesBuffer[i + (numBytes/2)*sampleBufferCnt];
+		}
+	}
+
+
+	sampleBufferCnt++;
+
+	if(sampleBufferCnt >= 50){
+
+		startReverb = true;
+		sampleBufferCnt = 0;
+	}
+
+/*	free(combFilter1Output);
+	free(combFilter2Output);
+	free(combFilter3Output);
+	free(combFilter4Output);
+	free(allPassFilter1Output);
+	free(allPassFilter2Output);*/
 }
 
 void Reverb::setIntensity(uint8_t intensity){
 
-	mixPercent = (intensity/236.0f) * 100;
-
+	mixPercent = ((float)intensity/236.0f) * 100;
+	//decayFactor = ((float)intensity/236.0f) * 100;
 }
 
 Reverb::~Reverb() noexcept{
 
-	free(combFilter1Output);
-	free(combFilter2Output);
-	free(combFilter3Output);
-	free(combFilter4Output);
+/*	free(allPassFilter1Output);
+	free(allPassFilter2Output);*/
 
-	free(allPassFilter1Output);
-	free(allPassFilter2Output);
 }
--- src/Reverb.h
+++ src/Reverb.h
@@ -1,13 +1,7 @@
-//
-// Created by Domagoj on 19/02/2021.
-//
-
 #ifndef JAYD_LIBRARY_REVERB_H
 #define JAYD_LIBRARY_REVERB_H
 
 #include "JayD.hpp"
-
-#define SAMPLE_RATE	44100
 
 class Reverb : public AudioEffect{
 
@@ -19,7 +13,6 @@ public:
 	void applyEffect(int16_t *inBuffer, int16_t *outBuffer, int numBytes) override;
 	void setIntensity(uint8_t intensity);
 
-
 private:
 
 	void combFilter(int16_t* sample, int numberOfSamples);
@@ -27,18 +20,25 @@ private:
 
 	void signalProcessing(int16_t* samples, int numBytes);
 
-	int32_t * combFilter1Output = nullptr;
-	int32_t * combFilter2Output = nullptr;
-	int32_t * combFilter3Output = nullptr;
-	int32_t * combFilter4Output = nullptr;
+	int16_t* samplesBuffer = nullptr;
+	uint8_t sampleBufferCnt;
+	bool startReverb;
+
+	int32_t* combFilter1Output = nullptr;
+	int32_t* combFilter2Output = nullptr;
+	int32_t* combFilter3Output = nullptr;
+	int32_t* combFilter4Output = nullptr;
 
 	int32_t * allPassFilter1Output = nullptr;
 	int32_t * allPassFilter2Output = nullptr;
 
-	float delaySamples = 78.9f;
-	float decayFactor = 0.45f;
+	float delaySamples;
+	float decayFactor;
 
-	uint8_t mixPercent = 50;
+	int delayAllPassSamples;
+	float allPassDecay;
+
+	uint8_t mixPercent;
 };
 
 
