c10f78b3e14b2597a55b7c49602186d22210f581
Domagoj <domagoj.zoraja@fer.hr>
2021-03-05 11:02:24 +0100

    time check

--- src/BitCrusher.cpp
+++ src/BitCrusher.cpp
@@ -7,10 +7,15 @@ BitCrusher::BitCrusher(){
 
 void BitCrusher::applyEffect(int16_t *inBuffer, int16_t *outBuffer, int numBytes){
 
+	uint32_t beginTime = micros();
+
 	for(int i = 0; i < numBytes/2; ++i){
 
 		outBuffer[i] = signalProcessing(inBuffer[i]);
 	}
+
+	printf("It takes %ld time to process %d samples.\n",micros()-beginTime, numBytes/2);
+
 }
 
 int16_t BitCrusher::signalProcessing(int16_t sample){
