68f4bc33023a5020ddc80bef6f1e7574fd7d9276
Domagoj <domagoj.zoraja@fer.hr>
2021-03-05 13:50:49 +0100

    scale factor update

--- src/BitCrusher.cpp
+++ src/BitCrusher.cpp
@@ -7,15 +7,10 @@ BitCrusher::BitCrusher(){
 
 void BitCrusher::applyEffect(int16_t *inBuffer, int16_t *outBuffer, int numBytes){
 
-	uint32_t beginTime = micros();
-
 	for(int i = 0; i < numBytes/2; ++i){
 
 		outBuffer[i] = signalProcessing(inBuffer[i]);
 	}
-
-	printf("It takes %ld time to process %d samples.\n",micros()-beginTime, numBytes/2);
-
 }
 
 int16_t BitCrusher::signalProcessing(int16_t sample){
@@ -28,9 +23,7 @@ int16_t BitCrusher::signalProcessing(int16_t sample){
 
 void BitCrusher::setIntensity(uint8_t intensity){
 
-	/// max intensity = 236
-
-	scaleFactor = (intensity / 237.0f) * pow(2, 14);
+	scaleFactor = (intensity / 255.0f) * pow(2, 14);
 	scaleFactor = max((uint16_t) 1, scaleFactor);
 
 }
