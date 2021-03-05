3a0af91a8eecc121a8b9d6389a94b3815ae40aef
Domagoj <domagoj.zoraja@fer.hr>
2021-03-05 11:03:42 +0100

    variables made float

--- src/Reverb.h
+++ src/Reverb.h
@@ -17,17 +17,17 @@ public:
 
 private:
 
-	int32_t signalProcessing(uint32_t index, uint32_t delay0, uint32_t delay1, uint32_t delay2, uint32_t delay3);
+	int16_t signalProcessing(uint32_t index, uint32_t delay0, uint32_t delay1, uint32_t delay2, uint32_t delay3);
 
-	int16_t* samplesBuffer = nullptr;
+	float* samplesBuffer = nullptr;
 	uint8_t sampleBufferCnt;
 
 	bool startReverb;
 	float decayFactor;
-	uint8_t mixPercent;
+	float mixPercent;
 
-	uint16_t maxAmp;
-	int32_t treshold;
+	float maxAmp;
+	float threshold;
 };
 
 
