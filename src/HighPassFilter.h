56d4b4cd90b67103267935009062ccab20608847
Domagoj <domagoj.zoraja@fer.hr>
2021-03-05 13:52:11 +0100

    HP optimization (simplified)

--- src/HighPassFilter.h
+++ src/HighPassFilter.h
@@ -1,14 +1,7 @@
-//
-// Created by Domagoj on 18/02/2021.
-//
-
 #ifndef JAYD_LIBRARY_HIGHPASSFILTER_H
 #define JAYD_LIBRARY_HIGHPASSFILTER_H
 
 #include "JayD.hpp"
-
-#define N 20		// filter order
-#define L 21		// length of the impulse response L = (N+1)
 
 class HighPassFilter: public AudioEffect {
 
@@ -20,28 +13,11 @@ public:
 
 	void setIntensity(uint8_t intensity) override;
 
-
 private:
-
-	void idealFilterResponse();
-	void windowResponse();
-
-	void generateFilterCoeffs();
-
-	long int circularIndex(long index, long increment, uint8_t irl);
 
 	int16_t signalProcessing(int16_t sample);
 
-	float cutOffFrequency = 0;
 	float gain = 0;
-
-	float coeffs[L] = {0};
-	float windowCoeffs[L] = {0};
-	float idealFilterCoeffs[L] = {0};
-
-	float fDelay[L] = {0};
-
-	int j = 0;
 
 	float filter;
 	float filter2;
@@ -49,7 +25,7 @@ private:
 	float val;
 	float fAmp;
 	float fAmpI;
-	float fAmpS;
+
 };
 
 
