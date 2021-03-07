f1ff3efb939409c3e1165517a0a595285e1ed037
Filip Budiša <filip@circuitmess.com>
2021-03-07 01:53:56 +0100

    Add i2s pin config to JayD.hpp

--- src/JayD.hpp
+++ src/JayD.hpp
@@ -2,14 +2,7 @@
 #define JAYD_H
 
 #include <Arduino.h>
-#include "AudioSetup.hpp"
-#include "AudioLib/Output.h"
-#include "AudioLib/EffectProcessor.h"
-#include "AudioLib/Mixer.h"
-#include "AudioLib/Converter.h"
-#include "AudioLib/SourceWAV.h"
-#include "AudioLib/SourceMP3.h"
-#include "AudioLib/OutputI2S.h"
-#include "AudioLib/OutputFS.h"
+#include <driver/i2s.h>
+extern const i2s_pin_config_t i2s_pin_config;
 
 #endif
