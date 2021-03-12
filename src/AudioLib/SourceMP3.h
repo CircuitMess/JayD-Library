eb9e62f283a652700bbf6f874a877b90d12d127b
Filip Budiša <filip@circuitmess.com>
2021-03-12 13:33:54 +0100

    Update SourceMP3

--- src/AudioLib/SourceMP3.h
+++ src/AudioLib/SourceMP3.h
@@ -4,21 +4,58 @@
 #include <Arduino.h>
 #include <FS.h>
 #include "Source.h"
+#include "Decoder/ID3Parser.h"
+#include "Decoder/mad/mad.h"
+#include "../AudioSetup.hpp"
+#include <Buffer/FSBuffer.h>
+#include <Buffer/DataBuffer.h>
+
+// 2106
+#define MP3_BUFFERSIZE 2106 * 3
+#define DATA_BUFFERSIZE (1156 * 2)
 
 class SourceMP3 : public Source
 {
 public:
-	SourceMP3();
-	SourceMP3(fs::File *file);
+	SourceMP3(File& file);
+	SourceMP3(const String& path);
 	~SourceMP3();
 	size_t generate(int16_t* outBuffer) override;
 	int available() override;
 
-	void open(fs::File *file);
+	void open(File& file);
+	void open(const String& path);
+
+	void close() override;
+
+	uint16_t getDuration() override;
+	uint16_t getElapsed() override;
+
+	const ID3Metadata& getMetadata() const;
+
+	void seek(uint16_t time, fs::SeekMode mode) override;
 
 private:
-	fs::File *file = nullptr;
+	File file;
 
+	FSBuffer* input = nullptr;
+	DataBuffer* output = nullptr;
+
+	ID3Metadata metadata;
+
+	size_t dataStart = 0;
+
+	mad_stream* stream = nullptr;
+	mad_frame* frame = nullptr;
+	mad_synth* synth = nullptr;
+	size_t samplesAvailable = 0;
+	size_t samplesRead = 0;
+
+	void decode();
+
+	void processSynth();
+
+	File openUnicodePath(const char* path);
 };
 
 
