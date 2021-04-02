b8fab58f8dd0c5e964f3ba057bdc073e8e6f3997
Domagoj <domagoj.zoraja@fer.hr>
2021-04-02 15:18:54 +0200

    add scaling factors for output signal

--- src/AudioLib/Effects/Reverb.cpp
+++ src/AudioLib/Effects/Reverb.cpp
@@ -11,17 +11,43 @@ Reverb::Reverb(){
 
 int16_t Reverb::signalProcessing(int16_t sample){
 
-	int16_t echoSample1 = (float)echo[(length + echoCount) % (length)] * echoAmount;
+	int16_t echoSample1 = (float) echo[(length + echoCount) % (length)] * echoAmount;
 
-	echo[echoCount++] = (float)sample;
-	if (echoCount >= length){
+	echo[echoCount++] = (float) sample;
+	if(echoCount >= length){
 		echoCount = 0;
 	}
 
-	sample+=echoSample1;
+	float maxAmp = pow(2,15)-1;
+	float threshold = maxAmp * 0.9;
+	float window = maxAmp - threshold;
 
-	return sample;
+	if(abs(sample+echoSample1)> 0x7FFF){
+
+		sample = (float)sample*(0.5f*((1 - echoAmount) + 1)) + (float)echoSample1*(echoAmount*0.5f);
+	}
+	else{
+		sample+=echoSample1;
+	}
+
+	if(sample < 0){
+
+		threshold = -threshold;
+	}
+
+	if(abs(sample) >= abs(threshold)){
+
+		float over = (float)sample - threshold;
+		float overC = over/window;
+
+		sample = threshold + over * cos(overC*M_PI_2);
+
+		return (int16_t)sample;
+	}
+	else
+		return (int16_t)sample;
 }
+
 
 void Reverb::applyEffect(int16_t *inBuffer, int16_t *outBuffer, size_t numSamples){
 	for(int i = 0; i < numSamples; ++i){
@@ -30,7 +56,7 @@ void Reverb::applyEffect(int16_t *inBuffer, int16_t *outBuffer, size_t numSample
 }
 
 void Reverb::setIntensity(uint8_t intensity){
-	echoAmount = (float)intensity/255.0f;
+	echoAmount = (float) intensity / 255.0f;
 }
 
 Reverb::~Reverb(){
