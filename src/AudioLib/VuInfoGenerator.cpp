3f0c001c7c02ee019a249181e28ab47cbbe7fb2c
Emil Gajšak <emil.gajsak@gmail.com>
2021-04-08 09:34:24 +0200

    Added RoundVuVisualiser, tweaked Vu levels

--- src/AudioLib/VuInfoGenerator.cpp
+++ src/AudioLib/VuInfoGenerator.cpp
@@ -2,12 +2,11 @@
 #include "../AudioSetup.hpp"
 
 void VuInfoGenerator::captureInfo(int16_t *outBuffer, size_t readBytes) {
-	float EMA_a = 0.02;
+	float EMA_a = 0.01;
 	vu = abs(outBuffer[0]);
 	for (int i = 1; i < readBytes; ++i) {
 		vu = (EMA_a * abs(outBuffer[i])) + ((1 - EMA_a) * vu);
 	}
-	Serial.println(vu);
 }
 
 uint16_t VuInfoGenerator::getVu() {
