90775b75c36ac4ae46422c2b4ed44a4a36ef6325
Emil Gajšak <emil.gajsak@gmail.com>
2021-04-13 00:48:29 +0200

    Added i2s running check before zeroing dma

--- src/AudioLib/Systems/MixSystem.cpp
+++ src/AudioLib/Systems/MixSystem.cpp
@@ -294,6 +294,8 @@ void MixSystem::seekChannel(uint8_t channel, uint16_t time) {
 
 void MixSystem::_seekChannel(uint8_t channel, uint16_t time) {
 	if (channel > 1) return;
-	i2s_zero_dma_buffer((i2s_port_t)0);
+	if(out->isRunning()) {
+		i2s_zero_dma_buffer((i2s_port_t) 0);
+	}
 	source[channel]->seek(time, SeekSet);
 }
